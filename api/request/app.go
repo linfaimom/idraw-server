3fd80e0779cf12502e1246d80acfb36675733364
林凤翔 <linfengxiang01@corp.netease.com>
2024-03-02 17:49:09 +0800

    image generation can now use dall-e-3 model

--- api/request/app.go
+++ api/request/app.go
@@ -8,6 +8,7 @@ type FileUploadReq struct {
 }
 
 type ImageGenerationReq struct {
+	Model  string `json:"model" binding:"required"`
 	User   string `json:"user" binding:"required"`
 	Prompt string `json:"prompt" binding:"required"`
 	N      int    `json:"n" binding:"gte=1,lte=10"`
